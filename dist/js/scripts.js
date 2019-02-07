ffd607284f9b25aac965a0be1678c50b78f761bc
Goodoose <ko.chernyshenko@gmail.com>
2019-02-07 15:22:33 +0200

    add folder dist

---
+++ dist/js/scripts.js
@@ -0,0 +1,7 @@
+// eslint-disable-next-line import/extensions
+import PhonesPage from './phone/phones-page.js';
+
+// eslint-disable-next-line no-unused-vars
+const currentPage = new PhonesPage({
+  element: document.querySelector('[data-component-main]'),
+});
